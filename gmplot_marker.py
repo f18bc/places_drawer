392227cc94931d059ccd8698c8501534137f8a0e
f18bc <42793728+f18bc@users.noreply.github.com>
2019-07-18 02:48:36 +0900

    Update gmplot_marker.py

--- gmplot_marker.py
+++ gmplot_marker.py
@@ -5,14 +5,21 @@ city = input('Enter city: ')
 b = geocoder.osm(city)
 gmap1 = gmplot.GoogleMapPlotter((b.latlng)[0], (b.latlng)[1], 13)
 
+c = 0
+cc = 0
+
 f = open("places.txt", "r")
 for line in f.readlines():
     line = line.strip()
     g = geocoder.osm(str(line))
     try:
         gmap1.marker(float(g.latlng[0]), float(g.latlng[1]), title = str(line))
+        c += 1
     except:
-        print(line)
+        print("Unable to search:", line)
+        cc += 1
         continue
-
-gmap1.draw("places.html")
+    cc += 1
+ccc = cc - c
+print("Failed to read ", ccc, "Places" )
+gmap1.draw("where.html")
